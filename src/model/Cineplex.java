f808b1b62b214e7038364c870ce6b02123c5fee1
ShaoWei <laushaowei@gmail.com>
2022-10-19 13:56:37 +0800

    Cineplex Version 1.0

--- src/model/Cineplex.java
+++ src/model/Cineplex.java
@@ -1,5 +1,63 @@
 package model;
 
-public class Cineplex {
+import java.util.ArrayList;
 
+/**
+ * The class that initialise a Cineplex.
+ * 
+ * @author Shao Wei
+ * @version 1.0
+ * @since 2022-10-19
+ */
+public class Cineplex {
+    /**
+     * Location of the Cineplex
+     */
+    private String location;
+
+    /**
+     * Number of cinemas in the Cineplex
+     */
+    private int numOfCinemas;
+
+    /**
+     * List of cinemas in the Cineplex
+     */
+    private ArrayList<Cinema> cinemaList;
+
+    /**
+     * Constructor of Cineplex
+     * 
+     * @param location    Location of Cineplex
+     */
+    public Cineplex(String location, int numOfCinemas){
+        this.location = location;
+        this.numOfCinemas = numOfCinemas;
+    }
+
+    /**
+     * Gets the location of the Cineplex
+     * 
+     * @return location of the Cineplex
+     */
+    public String getLocation(){
+        return this.location;
+    }
+
+    /**
+     * Gets the number of cinemas in the Cineplex
+     * 
+     * @return number of cinemas in the Cineplex
+     */
+    public int getNumOfCinemas(){
+        return this.numOfCinemas;
+    }
+
+    /**
+     * Displays the list of cinemas in the Cineplex
+     * 
+     */
+    public void showCinemaList(){
+        System.out.println(this.cinemaList);
+    }
 }
