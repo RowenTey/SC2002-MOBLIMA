dad885b54ffb40557f4395f2147e87a6f8fa7dc9
Yang Xiaoyue <104242725+amiyang@users.noreply.github.com>
2022-10-19 22:16:25 +0800

    Create AgeGroup.java

---
+++ src/model/enums/AgeGroup.java
@@ -0,0 +1,32 @@
+package model.enums
+
+/**
+ * An enum that stores the different agegroups of movie-goers 
+ * 
+ * @author Xiaoyue
+ * @version 1.0
+ * @since 2022-10-19
+ */
+ 
+public enum AgeGroup {
+   
+  /**
+   * ADULT AGEGROUP
+   */
+  ADULT,
+
+  /**
+   * CHILD AGEGROUP
+   */
+  CHILD,
+  
+  /**
+   * SENIOR AGEGROUP
+   */
+  SENIOR_CITIZEN,
+
+  /**
+   * STUDENT AGEGROUP
+   */
+  STUDENT
+}
