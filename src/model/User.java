de7059b221906a907b0eef6910af2acdda902f76
Yang Xiaoyue <104242725+amiyang@users.noreply.github.com>
2022-10-19 22:35:57 +0800

    Update User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,35 @@
 package model;
 
+/**
+ * The abstract class that stores the name of the user
+ * 
+ * @author Xiaoyue
+ * @version 1.0
+ * @since 2022-10-19
+ */
 public abstract class User {
+    /**
+     * Name of the user
+     */
+    private String userName;
+    
+    /**
+     * Sets the name of the user
+     *
+     * @param name name of the user
+     */
+    public void setName(String name)
+    {
+        this.userName = name;
+    }
 
+    /**
+     * Gets the name of the current user
+     *
+     * @return the name of the user
+     */
+    public String getName()
+    {
+        return this.userName;
+    }
 }
