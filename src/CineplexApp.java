962f44cd387e161cb0c280101735cb611707361b
RowenTey <kaiseong02@gmail.com>
2022-10-19 10:46:43 +0800

    Added logo

--- src/CineplexApp.java
+++ src/CineplexApp.java
@@ -1,5 +1,41 @@
 public class CineplexApp {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        printMOBLIMATitle();
     }
+
+    /**
+     * Prints the MOBLIMA title.
+     */
+    private static void printMOBLIMATitle() {
+        System.out.println();
+        System.out.println(
+                "╔═════════════════════════════════════════════════════════════════════════════════════════════════════╗");
+        System.out.println(
+                "║               __       __   ______   _______   __        ______  __       __   ______               ║");
+        System.out.println(
+                "║              /  \\     /  | /      \\ /       \\ /  |      /      |/  \\     /  | /      \\              ║");
+        System.out.println(
+                "║              ▐▐  \\   /▐▐ |/▐▐▐▐▐▐  |▐▐▐▐▐▐▐  |▐▐ |      ▐▐▐▐▐▐/ ▐▐  \\   /▐▐ |/▐▐▐▐▐▐  |             ║");
+        System.out.println(
+                "║              ▐▐▐  \\ /▐▐▐ |▐▐ |  ▐▐ |▐▐ |__▐▐ |▐▐ |        ▐▐ |  ▐▐▐  \\ /▐▐▐ |▐▐ |__▐▐ |             ║");
+        System.out.println(
+                "║              ▐▐▐▐  /▐▐▐▐ |▐▐ |  ▐▐ |▐▐    ▐▐< ▐▐ |        ▐▐ |  ▐▐▐▐  /▐▐▐▐ |▐▐    ▐▐ |             ║");
+        System.out.println(
+                "║              ▐▐ ▐▐ ▐▐/▐▐ |▐▐ |  ▐▐ |▐▐▐▐▐▐▐  |▐▐ |        ▐▐ |  ▐▐ ▐▐ ▐▐/▐▐ |▐▐▐▐▐▐▐▐ |             ║");
+        System.out.println(
+                "║              ▐▐ |▐▐▐/ ▐▐ |▐▐ \\__▐▐ |▐▐ |__▐▐ |▐▐ |_____  _▐▐ |_ ▐▐ |▐▐▐/ ▐▐ |▐▐ |  ▐▐ |             ║");
+        System.out.println(
+                "║              ▐▐ | ▐/  ▐▐ |▐▐    ▐▐/ ▐▐    ▐▐/ ▐▐       |/ ▐▐   |▐▐ | ▐/  ▐▐ |▐▐ |  ▐▐ |             ║");
+        System.out.println(
+                "║              ▐▐/      ▐▐/  ▐▐▐▐▐▐/  ▐▐▐▐▐▐▐/  ▐▐▐▐▐▐▐▐/ ▐▐▐▐▐▐/ ▐▐/      ▐▐/ ▐▐/   ▐▐/              ║");
+        System.out.println(
+                "║                                                                                                     ║");
+        System.out.println(
+                "║                         Welcome to Movie Booking and Reservationn System                            ║");
+        System.out.println(
+                "║                                                                                                     ║");
+        System.out.println(
+                "╚═════════════════════════════════════════════════════════════════════════════════════════════════════╝");
+    };
 }
