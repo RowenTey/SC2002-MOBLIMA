1fd1f509b2b800349fb2ac6ce66b35c3bbe0fdbe
ScatteredThunderstorms <angarthurace@yahoo.com.sg>
2022-10-20 22:27:12 +0800

    comments for review

--- src/model/Review.java
+++ src/model/Review.java
@@ -1,18 +1,53 @@
 package model;
 
+/**
+ * The class that customer review
+ * 
+ * @author Ace Ang
+ * @version 1.0
+ * @since 2022-10-20
+ */
+
 public class Review {
+
+    /**
+     * review ID for review
+     */
     private int reviewId;
+
+    /**
+     * string that contains the actual review
+     */
     private String review;
+
+    /**
+     * rating for the review
+     */
     private double rating;
 
+    /**
+     * Gets the ID of the booking
+     *
+     * @return the ID of the booking
+     */
     public int getReviewId() {
         return reviewId;
     }
 
+    /**
+     * Sets the ID of the booking
+     *
+     * @param reviewId ID of the booking
+     */
     public void setReviewId(int reviewId) {
         this.reviewId = reviewId;
     }
 
+    /**
+     * Gets the review
+     *
+     * @return the review
+     */
     public String getReview() {
         return review;
     }
