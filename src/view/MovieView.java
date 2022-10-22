9ef5dea55d41b83a6c3474d311b558439792387d
ShaoWei <laushaowei@gmail.com>
2022-10-22 16:24:52 +0800

    modified

--- src/view/MovieView.java
+++ src/view/MovieView.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import helper.Helper;
 
 /**
@@ -35,13 +38,23 @@ public class MovieView extends MainView {
     public void printMenu() {
         Helper.clearScreen();
         printRoute(this.path + " > Movies");
+        // TODO Movies.getList()
+        List<String> list=new ArrayList<String>();
+        list.add("One Piece Film Red");
+        list.add("Black Adam");
+        list.add("Fall");
+        list.add("Thor: Love and Thunder");
+
+
         System.out.println("List of movies (both \"NOW SHOWING\" and \"COMING SOON\")");
         // TODO: use for loop to list down the movies
-        System.out.println("(1) One Piece Film Red");
-        System.out.println("(2) Black Adam");
-        System.out.println("(3) Fall");
-        System.out.println("(4) Thor: Love and Thunder");
-        System.out.println("(5) Exit");
+        for(int i=0; i<list.size(); i++){
+            System.out.println("("+(i+1)+") "+ list.get(i));
+            if(i+1 == list.size()){
+                System.out.println("("+(i+2)+") Exit");
+            }
+        }
+        
         System.out.println("Which movie do you want to choose?");
     }
 
@@ -49,40 +62,29 @@ public class MovieView extends MainView {
      * View App
      */
     public void viewApp() {
+        // TODO Movies.getList()
+        List<String> list=new ArrayList<String>();
+        list.add("One Piece Film Red");
+        list.add("Black Adam");
+        list.add("Fall");
+        list.add("Thor: Love and Thunder");
         int choice = -1;
+
         do {
             this.printMenu();
-            choice = Helper.readInt(1, 5);
-            switch (choice) {
-                case 1:
-                    System.out.println("One Piece Film Red");
+            choice = Helper.readInt(0, (list.size()+1));
+            if(choice == list.size()+1){
+                break;
+            }else{
+                System.out.println(list.get(choice-1));
                     // TODO: MovieManager.showDetails() -> Synopsis, director, cast, overall rating,
                     // showtimes
                     // TODO: Prompt for BookView() or ReviewView()
-                    ReviewView reviewView = new ReviewView("One Piece Film Red", this.path+" > Movies");
+                    ReviewView reviewView = new ReviewView(list.get(choice-1), this.path+" > Movies");
                     reviewView.viewApp();
-                    break;
-                case 2:
-                    System.out.println("Black Adam");
-                    // TODO: MovieManager.showDetails() -> Synopsis, director, cast, overall rating,
-                    // showtimes
-                    break;
-                case 3:
-                    System.out.println("Fall");
-                    // TODO: MovieManager.showDetails() -> Synopsis, director, cast, overall rating,
-                    // showtimes
-                    break;
-                case 4:
-                    System.out.println("Thor: Love and Thunder");
-                    // TODO: MovieManager.showDetails() -> Synopsis, director, cast, overall rating,
-                    // showtimes
-                    break;
-                case 5:
-                    break;
-                default:
-                    break;
             }
-        } while (choice != 5);
+            
+        } while (choice != list.size()+1);
     }
 
 }
