f3edb9b1329d66bcecd4f3215022b59316c6bb4d
ShaoWei <laushaowei@gmail.com>
2022-10-22 17:13:19 +0800

    cineplexmanager

--- src/controller/CineplexManager.java
+++ src/controller/CineplexManager.java
@@ -1,5 +1,27 @@
 package controller;
+import helper.Helper;
+import java.util.ArrayList;
+
+import model.Cineplex;
+import model.enums.Location;
 
 public class CineplexManager {
-    
+    private static ArrayList<Cineplex> cineplexList = new ArrayList<Cineplex>();
+    private static int totalCineplex = 0;
+
+    public static void addCineplex(){
+        System.out.println("Where do you want add a new Cineplex ?");
+        for(int i=0; i<Location.values().length; i++){
+            System.out.println("("+(i+1)+") "+ Location.values()[i].getLabel());
+        }
+        int choice = Helper.readInt(1,Location.values().length+1);
+        Cineplex newCineplex = new Cineplex(Location.values()[choice-1]);
+        CineplexManager.cineplexList.add(newCineplex);
+    }
+    public static void removeCineplex(Cineplex cineplex){
+        CineplexManager.cineplexList.remove(cineplex);
+    }
+    public static ArrayList<Cineplex> getCineplexList(){
+        return CineplexManager.cineplexList;
+    }
 }
--- src/view/CineplexView.java
+++ src/view/CineplexView.java
@@ -1,5 +1,7 @@
 package view;
 
+
+import controller.CineplexManager;
 import helper.Helper;
 
 /**
@@ -34,11 +36,14 @@ public class CineplexView extends MainView {
     public void printMenu() {
         Helper.clearScreen();
         printRoute(this.path + " > Cineplex");
+        System.out.println(CineplexManager.getCineplexList());
         System.out.println("We have 3 Cineplexes in Singapore");
         System.out.println("(1) JEM");
         System.out.println("(2) Causeway Point");
         System.out.println("(3) AMK Hub");
         System.out.println("(4) Exit");
+        System.out.println("(5) Add Cineplex");
+        System.out.println("(6) View Cineplex");
         System.out.println("Which location would you like to choose? ");
     }
 
@@ -49,7 +54,7 @@ public class CineplexView extends MainView {
         int choice = -1;
         do {
             this.printMenu();
-            choice = Helper.readInt(1, 4);
+            choice = Helper.readInt(1, 6);
             switch (choice) {
                 case 1:
                     System.out.println("JEM selected...");
@@ -64,6 +69,9 @@ public class CineplexView extends MainView {
                     // TODO (showtimeView())
                     break;
                 case 4:
+                    break;
+                case 5:
+                    CineplexManager.addCineplex();
                     break;
                 default:
                     break;
