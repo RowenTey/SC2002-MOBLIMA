a60de88a2924d19e80f4198a8e76528468866e51
Yang Xiaoyue <104242725+amiyang@users.noreply.github.com>
2022-10-24 17:34:09 +0800

    added comments

--- src/model/ThreeDMovie.java
+++ src/model/ThreeDMovie.java
@@ -3,20 +3,41 @@ package model;
 import model.enums.ShowStatus;
 import model.enums.TypeMovies;
 
+/**
+ * The class that initialises the 3D movie
+ * 
+ * @author Xiaoyue
+ * @version 1.0
+ * @since 2022-10-24
+ */
 public class ThreeDMovie extends Movie {
 
+    /**
+     * the base price of the 3D movie
+     */
     private double price;
 
+    /**
+     * Constructor of 3D movie
+     * 
+     * @param price price of 3D movie
+     */
     public ThreeDMovie(String movieId, String title, ShowStatus status, String synopsis, String director, String[] cast,
             TypeMovies type, double price) {
         super(movieId, title, status, synopsis, director, cast, type);
         this.price = price;
     }
 
+    /**
+     * set the price of the 3D movie
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
+    /**
+     * get the price of the 3D movie
+     */
     public double getPrice() {
         return this.price;
     }
--- src/model/TwoDMovie.java
+++ src/model/TwoDMovie.java
@@ -3,22 +3,42 @@ package model;
 import model.enums.ShowStatus;
 import model.enums.TypeMovies;
 
+/**
+ * The class that initialises the 2D movie
+ * 
+ * @author Xiaoyue
+ * @version 1.0
+ * @since 2022-10-24
+ */
 public class TwoDMovie extends Movie {
 
+    /**
+     * the base price of the 2D movie
+     */
     private double price;
 
+    /**
+     * Constructor of 2D movie
+     * 
+     * @param price price of 2D movie
+     */
     public TwoDMovie(String movieId, String title, ShowStatus status, String synopsis, String director, String[] cast,
             TypeMovies type, double price) {
         super(movieId, title, status, synopsis, director, cast, type);
         this.price = price;
     }
 
+    /**
+     * set the price of the 2D movie
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
+    /**
+     * get the price of the 2D movie
+     */
     public double getPrice() {
         return this.price;
     }
-
 }
